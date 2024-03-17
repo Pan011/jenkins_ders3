9b8d49e80203598af4b85d0283377b2c99a5d497
Yusuf Cakiroglu <yusuf.cakiroglu11@gmail.com>
2024-03-17 14:19:09 +0000

    external script.groovy file added

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def runTests() {
+    def testsToRun = ['unit', 'integration', 'e2e']
+    for (testType in testsToRun) {
+        echo "Running ${testType} tests"
+        if (testType == 'e2e') {
+            echo 'Setting up Selenium Grid for e2e tests'
+            // Selenium Grid kurulum komutları
+        }
+        // Testleri çalıştırma komutları
+    }
+}
+
+return this
